3b486b0141cd6f3076dec732011adf834ca72fd6
congiu.gaetan <gaetan.congiu@esqese.ucly.fr>
2020-11-10 14:21:07 +0100

    Rajout de la scene menu

---
+++ src/scenes/MenuScene.js
@@ -0,0 +1,18 @@
+import Phaser from 'phaser'
+
+export default class MenuScene extends Phaser.Scene
+{
+    constructor()
+    {
+        super('menu-scene')
+    }
+    preload()
+    {
+        this.load.image('menu', './assets/menu.png')
+    }
+
+    create()
+    {
+        this.add.image(400, 300, 'menu')
+    }
+}
