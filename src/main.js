52aa0ed79bb7a7afa42f6424e40f2f46b64d347a
emmanuel-storm <61197043+emmanuel-storm@users.noreply.github.com>
2020-11-10 14:59:41 +0100

    resolution de conflit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Phaser from 'phaser'
 
-import HelloWorldScene from './scenes/HelloWorldScene'
+import GameScene from './scenes/GameScene'
 
 const config = {
 	type: Phaser.AUTO,
@@ -12,7 +12,7 @@ const config = {
 			gravity: { y: 200 }
 		}
 	},
-	scene: [HelloWorldScene]
+	scene: [GameScene]
 }
 
 export default new Phaser.Game(config)
---
+++ src/scenes/GameScene.js
@@ -0,0 +1,19 @@
+import Phaser from 'phaser'
+
+export default class GameScene extends Phaser.Scene
+{
+    constructor()
+    {
+        super('game-scene')
+    }
+
+    preload()
+    {
+        this.load.image('dude', './assets/dude.png')
+    }
+
+    create()
+    {
+        this.add.image(400, 300, 'dude')
+    }
+}
