e8b9bc0c2b96e63db883a1ee7c00584c1adc9f9e
clement.theotime <theotime.clement@esqese.ucly.fr>
2020-11-22 16:59:44 +0100

    modifier e bouton play

--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -16,6 +16,7 @@ export default class MenuScene extends Phaser.Scene
         this.load.image('menu', './assets/play.png')
 
 
+
     }
 
     create()
@@ -23,13 +24,27 @@ export default class MenuScene extends Phaser.Scene
 
 
 
+        //crée le rectangle rouge qui englobe le play
+        var color1 = new Phaser.Display.Color(250, 0, 180);
+        var rect1 = this.add.rectangle(480, 250, 20, 150, color1.color);
+
         //passer de la scéne menu a scéne game-scene
-        var start = this.add.image(600, 300, 'menu');
+        var start = this.add.text(500, 200, 'PLAY', { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif',fontSize:100, Color:"#FF0000"});
         start.setInteractive ({useHandCursor: true});
         start.on ('pointerdown', () => this.scene.start("game-scene"));
+        start.on ('pointerover', () =>  {rect1.width = 300});
+        start.on ('pointerout', () => {rect1.width = 20});
+
+
+
 
 
     }
+
+
+
+
+
 
 
 
