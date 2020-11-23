99007a611a5e1364cde1f9adcbdbc906bf74f046
clement.theotime <theotime.clement@esqese.ucly.fr>
2020-11-23 18:48:09 +0100

    ajout scene level

--- src/scenes/LevelsScene.js
+++ src/scenes/LevelsScene.js
@@ -1,25 +1,62 @@
 import Phaser from 'phaser'
+import GameScene from "./GameScene";
 
-export default class LevelsScene extends Phaser.Scene
+export default class Levelscene extends Phaser.Scene
 {
     constructor()
     {
-        super('levels-scene')
+        super('level-scene')
+
+
 
     }
 
     preload()
     {
-        // fait beleck
+
+
+
+
     }
 
     create()
     {
-        var level0 = this.add.text(1, 1, 'LEVEL_O', { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif',fontSize:50});
+
+
+        //titre de la page
+        var titrejeux = this.add.text(450, 10, 'LEVEL', { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif',fontSize:100});
+
+        //crée le rectangle pour un level
+        var color1 = new Phaser.Display.Color(250, 0, 0);
+        var rectlevel0 = this.add.rectangle(30, 230, 10, 80, color1.color);
+        var level0 = this.add.text(40, 200, 'LEVEL\n  0', { fontFamily: 'OCR A Std, monospace',fontSize:29});
         level0.setInteractive ({useHandCursor: true});
-        level0.on('pointerdown', () => this.scene.start("game-scene"));
+        level0.on ('pointerdown', () => this.scene.start("game-scene"));
+        level0.on ('pointerover', () =>  {rectlevel0.width = 100});
+        level0.on ('pointerout', () =>  {rectlevel0.width = 10});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
+
+
+
+
+
 
 
 
