d3bdd1cf02dc05b6ff2ee16783f41ffcc2d68cd9
zelnio.pierre <pierre.zelnio@esqese.ucly.fr>
2020-12-01 10:05:44 +0100

    je vire ce qui me gene sur ma branche

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -6,7 +6,6 @@ export default class GameScene extends Phaser.Scene
 	constructor()
 	{
 		super('game-scene');
-		this.score = 0;
 	}
 
 
@@ -26,9 +25,6 @@ export default class GameScene extends Phaser.Scene
 
     create()
     {
-
-        var score = 1;
-        let conteur = 1;
         let chaine_reponse_niveau_1 = "RRRRRRRRRR";
         let chaine_joueur = "";
 
@@ -40,40 +36,14 @@ export default class GameScene extends Phaser.Scene
 
 
         this.add.image(80, 50, 'score');
-       var Losange2Triche = this.add.image(1000, 1000, 'losange');
         this.btnCarre = this.add.sprite(417, 480, 'carre').setInteractive({useHandCursor: true});
         this.btnCircle = this.add.sprite(833, 480, 'circle').setInteractive({useHandCursor: true});
-        //this.btnLosange = this.add.sprite(723.5, 480, 'losange').setInteractive({useHandCursor: true});
-        //this.btnTriangle = this.add.sprite(906.5, 480, 'triangle').setInteractive({useHandCursor: true});
         this.button_menu = this.add.sprite(1170, 80, 'button_menu').setInteractive({useHandCursor: true});
-        this.printScore = this.add.text(80, 80, score, {  });
 
         var color1 = new Phaser.Display.Color(250, 0, 0);
-        //var rectLevel = this.add.rectangle(417, 480, 100, 100, color1.color);
-        var rect1 = this.add.rectangle(0, 0, 0, 0, color1.color);
-        var rect2 = this.add.rectangle(0, 0, 0, 0, color1.color);
-        var rect3 = this.add.rectangle(0, 0, 0, 0, color1.color);
-        //rectLevel.setInteractive ({useHandCursor: true});
-        //rectLevel.on('pointerdown', () => conteur += 1);
-        if( conteur === 0){
-            this.btnCarre.on('pointerdown', () => {rect2.x = 550,rect2.y = 430, rect2.height = 100, rect2.width = 100});
-
-
-        }else if (conteur === 1){
-            this.btnCarre.on('pointerdown', () => {rect2.x = 550,rect2.y = 300, rect2.height = 100, rect2.width = 100});
-            this.btnCarre.on('pointerdown', () => {rect1.x = 550,rect1.y = 430, rect1.height = 100, rect1.width = 100});
-        }else if (conteur === 2){
-            this.btnCarre.on('pointerdown', () => {rect2.x = 550,rect2.y = 170, rect2.height = 100, rect2.width = 100});
-            this.btnCarre.on('pointerdown', () => {rect1.x = 550,rect1.y = 300, rect1.height = 100, rect1.width = 100});
-            this.btnCarre.on('pointerdown', () => {rect3.x = 550,rect3.y = 430, rect3.height = 100, rect3.width = 100});
-        }
-
-
-
 
 
         //create button menu
-
 
 
         this.button_menu.on('pointerover', function () {
@@ -134,14 +104,7 @@ export default class GameScene extends Phaser.Scene
             }
         });
 
-        //create a the other forms with a rightclick
 
-        /*this.btnCarre.on('pointerdown', function (pointer) {
-
-            this.add.image(pointer.x, pointer.y, 'losange');
-            console.log("ajoute un losange")
-
-    });*/
         this.input.mouse.disableContextMenu();
 
         this.input.on('pointerdown', function (pointer) {
@@ -164,59 +127,6 @@ export default class GameScene extends Phaser.Scene
                 }
             }
         }, this);
-
-        //create button losange
-
-/*
-        this.btnLosange.on('pointerover', function () {
-            this.setTint(0x999999);
-        });
-        this.btnLosange.on('pointerout', function () {
-            this.clearTint();
-        });
-        this.btnLosange.on('pointerdown', function () {
-
-
-            this.setTint(0x444444);
-            console.log("salut");
-            //Losange2Triche.x = 630;
-            //Losange2Triche.y = 400;
-            chaine_joueur += "L";
-            console.log(chaine_joueur);
-            var position = chaine_joueur.indexOf(chaine_reponse_niveau_1);
-            if( position !== -1){
-                alert("C'est gagné !!")
-            }
-            else{
-                console.log("pas encore win")
-            }
-        });
-
-*/
-        //create button triangle
-
-/*
-        this.btnTriangle.on('pointerover', function () {
-            this.setTint(0x999999);
-        });
-        this.btnTriangle.on('pointerout', function () {
-            this.clearTint();
-        });
-        this.btnTriangle.on('pointerdown', function () {
-            this.setTint(0x444444);
-            chaine_joueur += "T";
-            console.log(chaine_joueur);
-            var position = chaine_joueur.indexOf(chaine_reponse_niveau_1);
-            if( position !== -1){
-                alert("C'est gagné !!")
-            }
-            else{
-                console.log("pas encore win")
-            }
-
-        });
-*/
-
 
 
     }
