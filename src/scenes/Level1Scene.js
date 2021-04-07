0154bcdab4d64c1a99dd04c29bdd16369d461b7b
zelnio.pierre <pierre.zelnio@esqese.ucly.fr>
2021-04-07 11:16:04 +0200

    Level 1 bug sur les autres machines

--- src/scenes/Level1Scene.js
+++ src/scenes/Level1Scene.js
@@ -1,21 +1,16 @@
 import Phaser from 'phaser'
 
 
-
 let score = 0;
-let nombreCompteur = 0;
-let posSprites = [];
-let tableau_feedback = [];
-export default class Level1Scene extends Phaser.Scene
-{
-    constructor()
-    {
+let pierre;
+
+export default class Level1Scene extends Phaser.Scene {
+    constructor() {
         super('level1-scene');
     }
 
 
-    preload()
-    {
+    preload() {
         //scene.load.plugin('rexcanvasplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexcanvasplugin.min.js', true);
 
         this.load.image('carre', './Whiteform/carre.png');
@@ -35,40 +30,36 @@ export default class Level1Scene extends Phaser.Scene
         this.load.image("win", "./game-icons/win.jpg");
 
 
-
     }
 
 
+    create() {
 
 
-    create()
-    {
 
-        //engrenage boutton
-        var engrenage = this.button_menu = this.add.sprite(1170, 80, 'button_menu');
-        engrenage.setInteractive ({useHandCursor: true});
-        engrenage.on ('pointerdown', () => this.scene.start("menu-scene"));
-
-        this.varwin = this.functwin();
-
-
-        this.add.text(500, 0, 'LEVEL 0', { fontFamily: 'OCR A Std, monospace', fontSize: 64});
-        this.add.text(500, 0, 'LEVEL 0', { fontFamily: 'OCR A Std, monospace', fontSize: 64});
+        this.add.text(500, 0, 'LEVEL 1', {fontFamily: 'OCR A Std, monospace', fontSize: 64});
         this.add.image(80, 50, 'score');
         this.btnCarre = this.add.sprite(417, 480, 'carre').setInteractive({useHandCursor: true});
         this.btnCircle = this.add.sprite(833, 480, 'circle').setInteractive({useHandCursor: true});
+        this.button_menu = this.add.sprite(1170, 80, 'button_menu').setInteractive({useHandCursor: true});
+
+
 
 
         const POINTER_DOWN = "pointerdown";
 
-        const hedonist = [[1], [0]];
+        const hedonist = [[1, -1], [1, -1]];
 
-        let score = 0;
+
         let nombreCompteur = 0;
+        let score = 0;
 
         let posSprites = [];
         let posValeurInterraction = [];
         let tableau_feedback = [];
+        let tableau_interaction = [];
+        let nos_noms = ['Pierre Zelnio', 'Gaétan Congiu', 'Emmanuel Lattier', 'Rami Johnstone', 'Valentin Favre', 'Théotime Clément'];
+        pierre = this.add.text(500, 520, 'Pierre', {fontFamily: 'OCR A Std, monospace', fontSize: 16});
 
         let rouge = new Phaser.Display.Color(250, 0, 0);
 
@@ -100,8 +91,6 @@ export default class Level1Scene extends Phaser.Scene
         //create button circle
 
 
-
-
         this.btnCircle.on('pointerover', function () {
             this.setTint(0x999999);
         });
@@ -117,11 +106,9 @@ export default class Level1Scene extends Phaser.Scene
 
         this.input.on('pointerdown', function (pointer) {
 
-            if (pointer.getDuration() > 1000)
-            {
+            if (pointer.getDuration() > 1000) {
 
-                if (pointer.rightButtonDown())
-                {
+                if (pointer.rightButtonDown()) {
                     if (pointer.x >= 300 && pointer.x <= 520) {
                         this.add.image(300, 250, 'circle');
                         this.add.image(500, 250, 'losange');
@@ -140,21 +127,30 @@ export default class Level1Scene extends Phaser.Scene
 
         let compteur = this.add.text(100, 100, "Nombre de coups =");
         let afficheScore = this.add.text(100, 120, "Score =");
+        let textWin = this.add.text(100, 140, "", { font: "60px calibri", fill: "orange"});
 
 
 
-        function Increment(){
+
+
+        function Increment() {
             compteur.setText([
                 'Nombre de coups = ' + nombreCompteur
             ]);
             afficheScore.setText([
                 'Score = ' + score
             ]);
+            if(score === 10){
+                afficheScore.setFill(['lime']);
+                textWin.setText([
+                    'Victoire !!'
+                ]);
+            }
         }
 
-        function feedback(parametre){
-            for(let i=0; i < tableau_feedback.length; i++){
-                if(i >= 9){
+        function feedback(parametre) {
+            for (let i = 0; i < tableau_feedback.length; i++) {
+                if (i >= 9) {
                     tableau_feedback.shift();
                 }
             }
@@ -166,40 +162,49 @@ export default class Level1Scene extends Phaser.Scene
 
         function calculScore() {
             score = 0;
-            for(let i =0; i < tableau_feedback.length; i++){
+            for (let i = 0; i < tableau_feedback.length; i++) {
                 score += tableau_feedback[i];
             }
             console.log(score);
-            if(score === 10){
-                //alert("Gagné !!")
 
-                //this.scene.start('level1-scene');
-                //////////////////////////////////////alert("Gagné !!");
-
-
-            }
         }
 
 
 
         this.btnCarre.on(POINTER_DOWN, () => {
 
-            if(posSprites.length > 0){
-                for(let i=0; i < posSprites.length; i++){
+            if (posSprites.length > 0) {
+                for (let i = 0; i < posSprites.length; i++) {
                     let sprite2posSprites = posSprites[i];
                     sprite2posSprites.y -= 40;
                     let toto = posValeurInterraction[i];
                     toto.y -= 40;
                 }
             }
+            let sprite;
+            let valeurInterraction;
 
-            let sprite = this.add.image(622, 400, 'carre_vert');
-            let valeurInterraction = this.add.text(645, 390, ""  + hedonist[0][0]);
+            if(tableau_interaction[tableau_interaction.length - 1] === 'Carré'){
+                feedback(hedonist[0][1]);
+                sprite = this.add.image(622, 400, 'carre_rouge');
+                valeurInterraction = this.add.text(645, 390, "" + hedonist[0][1]);
+            }
+            else{
+                feedback(hedonist[0][0]);
+                sprite = this.add.image(622, 400, 'carre_vert');
+                valeurInterraction = this.add.text(645, 390, "" + hedonist[0][0]);
+            }
+
+
+
 
             posSprites.push(sprite);
             posValeurInterraction.push(valeurInterraction);
             nombreCompteur += 1;
-            feedback(hedonist[0][0]);
+            tableau_interaction.push('Carré');
+            console.log(tableau_interaction);
+
+
             calculScore();
             Increment();
 
@@ -207,8 +212,8 @@ export default class Level1Scene extends Phaser.Scene
 
         this.btnCircle.on(POINTER_DOWN, () => {
 
-            if(posSprites.length > 0){
-                for(let i=0; i < posSprites.length; i++){
+            if (posSprites.length > 0) {
+                for (let i = 0; i < posSprites.length; i++) {
                     let sprite2posSprites = posSprites[i];
                     sprite2posSprites.y -= 40;
                     let toto = posValeurInterraction[i];
@@ -216,15 +221,33 @@ export default class Level1Scene extends Phaser.Scene
                 }
             }
 
-            let sprite = this.add.image(622, 400, 'circle_jaune');
-            let valeurInterraction = this.add.text(645, 390, "" + hedonist[1][0]);
+
+            let sprite;
+            let valeurInterraction;
+
+
+            if(tableau_interaction[tableau_interaction.length - 1] === 'Rond'){
+                feedback(hedonist[1][1]);
+                sprite = this.add.image(622, 400, 'circle_rouge');
+                valeurInterraction = this.add.text(645, 390, "" + hedonist[1][1]);
+            }
+            else{
+                feedback(hedonist[1][0]);
+                sprite = this.add.image(622, 400, 'circle_vert');
+                valeurInterraction = this.add.text(645, 390, "" + hedonist[1][0]);
+            }
+
+
 
 
             posSprites.push(sprite);
             posValeurInterraction.push(valeurInterraction);
-
             nombreCompteur += 1;
-            feedback(hedonist[1][0]);
+            tableau_interaction.push('Rond');
+            console.log(tableau_interaction);
+
+
+
             calculScore();
             Increment();
         });
@@ -233,20 +256,14 @@ export default class Level1Scene extends Phaser.Scene
     }
 
 
+    update(time, delta) {
 
 
-
-    update(time, delta)
-    {
-
-    }
-
-
-    functwin() {
-        if (score === 10) {
-
-            this.add.image(625, 225, "win");
-        }
+        pierre.x += 1;
 
     }
+
+
+
 }
+
