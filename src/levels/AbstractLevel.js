b47303834c73ddbb994027b32b8ac76c6cccb6da
Olivier Georgeon <olivier.georgeon@gmail.com>
2021-07-28 16:50:04 +0200

    delete levelswitch.js

--- src/levels/AbstractLevel.js
+++ src/levels/AbstractLevel.js
@@ -4,7 +4,6 @@ export default class AbstractLevel extends Phaser.Scene {
 
     constructor(level) {
         super(level);
-        //this.victoireMsg = 'Victoire ! Click here to access next level';
     }
 
     preload(){}
@@ -13,8 +12,21 @@ export default class AbstractLevel extends Phaser.Scene {
     
     update(){}
 
-    static getVictoireMessage(){
-        return 'Victoire! Click here to access next level';
+        //------------------------------------------------------------------------------------------------------------------------
+        //----------------------------------------PRINT SCORE & TEXT -------------------------------------------------------------
+        //------------------------------------------------------------------------------------------------------------------------
+        Increment(score, afficheScore, textWin) {
+        afficheScore.setText([
+            score
+        ]);
+        afficheScore.setFill(['white']);
+        if (score >= 10) {
+            afficheScore.setFill(['lime']);
+            textWin.setText([
+                'Victoire! Click here to access next level'
+            ]);
+            textWin.setStroke('#ffd700');
+        }
     }
 
 }
