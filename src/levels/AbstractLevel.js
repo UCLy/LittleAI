6b71565c4299ffb7f6baab4c9331fa04a42e74ef
Olivier Georgeon <olivier.georgeon@gmail.com>
2021-07-28 10:57:48 +0200

    delete levelswitch.js

---
+++ src/levels/AbstractLevel.js
@@ -0,0 +1,18 @@
+import phaser from 'phaser';
+
+export default class AbstractLevel extends Phaser.Scene {
+
+    //victoireMsg = 'Victoire ! Click here to access next level';
+
+    constructor() {
+        super('AbstractLevel');
+        //this.victoireMsg = 'Victoire ! Click here to access next level';
+    }
+
+    preload(){}
+
+    create() {}
+    
+    update(){}
+
+}
